21f06b6258105308ca5c46a8cd82f6ad85998ca9
MobGian <97670196+MobGian@users.noreply.github.com>
2022-01-27 13:19:18 +0100

    Add files via upload
    LoggedinController, SignupController updated. Importante FXML buttons e text layouts abbiano lo stesso id sia in java che in window builder

--- LoggedinController.java
+++ LoggedinController.java
@@ -13,7 +13,7 @@ import java.util.ResourceBundle;
 public class LoggedinController implements Initializable{
 
     @FXML
-    private Button button_logout; //collegato al FXML
+    private Button button_signout; //collegato al FXML
 
     @FXML
     private Label label_welcome;
@@ -21,8 +21,8 @@ public class LoggedinController implements Initializable{
 
     //listener logout button, cambia in login screen
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
-        button_logout.setOnAction(new EventHandler<ActionEvent>() {
+    public void initialize(URL location, ResourceBundle resourceBundle) {
+        button_signout.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
                 DBUtil.changeScene(actionEvent, "sample.fxml", "Log in!", "null" ); //logout porta alla pagina di accesso sample.fxml

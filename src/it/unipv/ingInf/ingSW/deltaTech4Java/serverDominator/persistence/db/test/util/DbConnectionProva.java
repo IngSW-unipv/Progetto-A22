e28511ea58ed2b37aed10dbec2cc9a48d003261e
TawaHabib <HabibSmail@myDomain.org>
2022-01-31 16:51:02 +0100

    res refactor

--- src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/persistence/db/test/util/DbConnectionProva.java
+++ src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/persistence/db/test/util/DbConnectionProva.java
@@ -7,11 +7,11 @@ import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.db.util.
 public class DbConnectionProva {
 
 	public static void main(String[] args) {
-		try {
+		
 			System.out.println("1");
 			Connection con=null;
 			System.out.println("2");
-			con=DbConnection.startConnection(con,"resorces/config.txt");
+			con=DbConnection.startConnection(con,"resources/config/dbConfig/connWith_sd_sys");
 			System.out.println("3");
 			if(DbConnection.isOpen(con)) {
 				System.out.println("4")
@@ -19,9 +19,20 @@ public class DbConnectionProva {
 			System.out.println("5");
 			DbConnection.closeConnection(con);
 			System.out.println("6");
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		
+		
+			System.out.println("1");
+			Connection conn=null;
+			System.out.println("2");
+			con=DbConnection.startConnection(conn,"resources/config/dbConfig/connWith_root");
+			System.out.println("3");
+			if(DbConnection.isOpen(conn)) {
+				System.out.println("4")
+				;}
+			System.out.println("5");
+			DbConnection.closeConnection(conn);
+			System.out.println("6");
+	
 	}
 
 }

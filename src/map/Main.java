99b9845d05fd257d48900ca16c1eba2d55012488
para4d <matteo.para01@universitadipavia.it>
2022-02-03 19:02:23 +0100

    inserita mappa esagoni

---
+++ src/map/Main.java
@@ -0,0 +1,13 @@
+package map;
+
+//import graphic.GraphicInterface;
+
+public class Main {
+
+	public static void main(String[] args) {
+		GraphicInterface gr = new GraphicInterface();
+	}
+	
+
+	
+}

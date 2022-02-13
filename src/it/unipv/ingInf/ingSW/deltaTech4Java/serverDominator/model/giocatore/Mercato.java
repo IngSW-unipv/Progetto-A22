7c10b51173a56b4f2742cfbd6c8d518a41c071c3
MatteoC <matteo.caprio01@universitadipavia.it>
2022-02-13 19:45:00 +0100

    prova

---
+++ src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/model/giocatore/Mercato.java
@@ -0,0 +1,15 @@
+package it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.giocatore;
+
+public class Mercato {
+	int prezzoRam, prezzoCPU, prezzoEnergia, prezzoVirus, prezzoAntivirus, prezzoRootcrash;
+	
+	public Mercato() {
+		prezzoRam=0;
+		prezzoCPU=0; 
+		prezzoEnergia=0;
+		prezzoVirus=0;
+		prezzoAntivirus=0;
+		prezzoRootcrash=0;
+	}
+	
+}

f123c4d57a5d89587b263ba1a7cde485750a66b7
TawaHabib <tawadrosgirgis.habib01@universitadipavia.it>
2022-04-22 17:24:32 +0200

    observerPunteggio

---
+++ src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/model/observers/ObserverPunteggio.java
@@ -0,0 +1,26 @@
+package it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.observers;
+
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.PersistenceFacade;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.bean.UserAccount;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.giocatore.Giocatore;
+
+public class ObserverPunteggio implements PropertyChangeListener {
+	private UserAccount osOsservato;
+	
+	
+	public ObserverPunteggio(UserAccount osOsservato) {
+		super();
+		this.osOsservato = osOsservato;
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		PersistenceFacade.getInstance().updateUserAccountPunteggio(osOsservato, (int)evt.getNewValue());
+		osOsservato=PersistenceFacade.getInstance().getUserAccountById(osOsservato);
+		Giocatore g=(Giocatore)evt.getSource();
+		g.aggiornaPunteggio(osOsservato.getMny()-g.getValuta());
+	}
+}

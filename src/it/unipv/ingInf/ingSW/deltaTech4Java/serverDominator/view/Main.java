15b052ccc726f86d2ec7b9e96739508d953a46bb
TawaHabib <tawadrosgirgis.habib01@universitadipavia.it>
2022-05-16 21:40:48 +0200

    error Main

--- src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/view/Main.java
+++ src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/view/Main.java
@@ -157,7 +157,6 @@ public class Main extends Application {
 				
 				sPane.getTitleL().setText("Stats Node: " + est.getX() + " , " + est.getY());
 				sPane.getOwner().setText("Owner: " + nodo.getPossessore().getNome());
-				sPane.getDistance().setText("Base distance: " + nodo.getDist_base());
 				sPane.getEnergy().setText("Energy: " + nodo.getE_disponibile());
 				sPane.getFwLvl().setText("Firewall Level: " + String.valueOf(nodo.getLvl_firewall()));
 				sPane.getRamLvl().setText("Ram Level: " + String.valueOf(nodo.getLvl_ram()));

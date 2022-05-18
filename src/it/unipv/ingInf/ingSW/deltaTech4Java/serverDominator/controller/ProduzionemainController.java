856ebdc4f6f4117bc60684242b9bb7f778fc686b
MobGian <97670196+MobGian@users.noreply.github.com>
2022-05-18 15:12:09 +0200

    Add files via upload

--- src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/controller/ProduzionemainController.java
+++ src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/controller/ProduzionemainController.java
@@ -2,6 +2,8 @@ package it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator;
 
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.MainDefinitivo;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.PersistenceFacade;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.bean.Asset;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.bean.UserAccount;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.partita.PartitaStage;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.partita.popUp.PopUpFacade;
 
@@ -20,11 +22,26 @@ public class ProduzionemainController {
 		return produzionemainController;
 	}
 	
+	/**
+	 * 
+	 * @param persistenceFacade
+	 * @param popupFacade
+	 * @param partitaStage
+	 * Metodo associato al pulsante di conferma di produzione risorsa, nel pop up di azione 
+	 */
 	
-	public void initProduzioneMain(PersistenceFacade persistenceFacade, PopUpFacade popupFacade,PartitaStage partitaStage) {
+	public void initProduzioneMain(PersistenceFacade persistenceFacade, PopUpFacade popupFacade,PartitaStage partitaStage, MainDefinitivo mainDefinitvo, UserAccount userAccount, Asset asset) {
 		popupFacade.getPopUpDevelopment().getButtonDevelop().setOnAction(actionEvent -> {
-			partitaStage.addProduzioneSoftware(null, 0); //String titolo e durata come parametri
+			partitaStage.addProduzioneSoftware(partitaStage.getSelectedNode().getPossessore().getNome(),
+			mainDefinitvo.softcheck(partitaStage.getSelectedPoint().getIntX(), partitaStage.getSelectedPoint().getIntY()),
+			userAccount.getAsetOwns().add(asset)
+			,0); //richiamo getAsset non corretto e durata mancante
 			
+			
+			//mainDefinitvo.creazioneSoftware(partitaStage.getSelectedBase().getPossessore(), persistenceFacade.getAssetOwndByUserAccount()
+			mainDefinitvo.creazioneSoftware(null, 0, 
+					
+					partitaStage.getSelectedPoint().getIntX(), partitaStage.getSelectedPoint().getIntY());// nome ,quantita e coordinate
 			
 		});
 	}

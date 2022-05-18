7e1672151fa0f9fb15bedad408953501575210a1
MobGian <97670196+MobGian@users.noreply.github.com>
2022-05-18 17:25:04 +0200

    Add files via upload

--- src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/controller/PotenziamentomainController.java
+++ src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/controller/PotenziamentomainController.java
@@ -2,9 +2,14 @@ package it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.controller;
 
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.Base;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.MainDefinitivo;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.risorse.Cpu;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.risorse.Firewall;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.risorse.Ram;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.PersistenceFacade;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.bean.Asset;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.bean.UserAccount;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.partita.PartitaStage;
-import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.partita.PopUpFacade;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.partita.popUp.PopUpFacade;
 
 public class PotenziamentomainController {
 	private static PotenziamentomainController potenziamentomainController = null;
@@ -22,16 +27,39 @@ public class PotenziamentomainController {
 	}
 	
 	
-	public void initPotenziamento(MainDefinitivo mainDefinitivo, PopUpFacade popupFacade, PartitaStage partitaStage, UserAccount userAccount,Base base) {
-		popupFacade.getPopUpPowerup().getButtonPowerUp().setOnAction(actionEvent-> {
-			partitaStage.addPtenziamentoRisorsa("POTENZIAMENTO RAM, BASE:\t,
+
+	
+	public void initPotenziamento (PartitaStage partitaStage, MainDefinitivo mainDefinitivo, PopUpFacade popupFacade, UserAccount userAccount, PersistenceFacade persistenceFacade, Ram ram, Cpu cpu, Firewall fireWall) {
+		popupFacade.getPopUpPowerup().getButtonPowerUp().setOnAction(actionEvent -> { //RAM
+			partitaStage.addPtenziamentoRisorsa("POTENZIAMENTO RAM,BASE \t",
 					mainDefinitivo.powerupCheck(partitaStage.getSelectedPoint().getIntX(), partitaStage.getSelectedPoint().getIntY()),
-					mainDefinitivo.powerup(partitaStage.getSelectedPoint().getIntX(), partitaStage.getSelectedPoint().getIntY(), 
-							userAccount.addAssetOwn(popupFacade.avviaDevelopment(null))) //DA CORREGGERE
-					
-					, 0); //TO-DO da inserire i parametri, String titolo battaglia e temppo
-			mainDefinitivo.powerup(partitaStage.getSelectedPoint().getIntX(), partitaStage.getSelectedPoint().getIntY(), null);
+					persistenceFacade.getAssetOwndByUserAccount(userAccount.addAssetOwn(ram.getLivello_risorsa())) //ERROR RICHIAMO addAssetOwn, che stat bisogna richiamare? getRichiesta
+					,ram.getTempo_richiesto()); //getTempoRichiesto() dal modello
+			
+			mainDefinitivo.powerup(partitaStage.getSelectedPoint().getIntX(), partitaStage.getSelectedPoint().getIntY(), ram.); //risorsa
 		});
+		
+		
+		popupFacade.getPopUpPowerup().getButtonPowerUp().setOnAction(actionEvent -> { //CPU
+			partitaStage.addPtenziamentoRisorsa("POTENZIAMENTO CPU, BASE \t",
+					mainDefinitivo.powerupCheck(partitaStage.getSelectedPoint().getIntX(), partitaStage.getSelectedPoint().getIntY()),
+					persistenceFacade.getAssetOwndByUserAccount(userAccount.addAssetOwn(cpu.getLivello_risorsa()))
+					,cpu.getTempo_richiesto());
+			
+			mainDefinitivo.powerup(partitaStage.getSelectedPoint().getIntX(), partitaStage.getSelectedPoint().getIntY(), cpu.)
+		});
+	
+		popupFacade.getPopUpPowerup().getButtonPowerUp().setOnAction(actionEvent -> { //FIREWALL
+			partitaStage.addPtenziamentoRisorsa("POTENZIAMENTO FIREWALL,BASE \t",
+					mainDefinitivo.powerupCheck(partitaStage.getSelectedPoint().getIntX(), partitaStage.getSelectedPoint().getIntY()),
+					persistenceFacade.getAssetOwndByUserAccount(userAccount.addAssetOwn(fireWall.getLivello_risorsa()))) //ERROR RICHIAMO addAssetOwn, che stat bisogna richiamare? getRichiesta
+					,fireWall.getTempo_richiesto())); //getTempoRichiesto() dal modello
+			
+			mainDefinitivo.powerup(partitaStage.getSelectedPoint().getIntX(), partitaStage.getSelectedPoint().getIntY(), fireWall.); //risorsa
+		});
+		
+		
+		
 	}
 	
 }

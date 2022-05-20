c652722040a012a6c764f2517224d5aa2ba278b0
MobGian <97670196+MobGian@users.noreply.github.com>
2022-05-20 10:18:55 +0200

    Add files via upload

---
+++ src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/controller/BaseCambioController.java
@@ -0,0 +1,53 @@
+package it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.controller;
+
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.Base;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.MainDefinitivo;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.giocatore.Giocatore;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.partita.PartitaStage;
+
+public class BaseCambioController {
+
+	private static BaseCambioController basecambioController = null;
+	private MainDefinitivo mainDefinitivo;
+	private BaseCambioController() {
+		super();
+	}
+	
+	public static BaseCambioController getIstance() {
+		if(basecambioController == null)
+			basecambioController = new BaseCambioController();
+		
+		return basecambioController;
+	}
+	
+	/**
+	 * 
+	 * @param partitaStage
+	 * @param b
+	 * @param giocatore
+	 * Metodo associato al pulsante per visualizzare la prossima base posseduta dal giocatore
+	 */
+	public void initAvantiBase(PartitaStage partitaStage, Base b, Giocatore giocatore) {
+		partitaStage.getBaseStatsPane().getButtonNext().setOnAction(actionEvent -> {
+			mainDefinitivo.getTabellone().checkbasi(giocatore);
+			mainDefinitivo.getTabellone().trovaBase(giocatore);
+			partitaStage.changeSelectedBase(b);
+		});
+	}
+	
+	/**
+	 * 
+	 * @param partitaStage
+	 * @param b
+	 * @param giocatore
+	 * Metodo associato al pulsante per visualizzare la precedente base posseduta dal giocatore
+	 */
+	public void iniIndietroBase(PartitaStage partitaStage, Base b, Giocatore giocatore) {
+		partitaStage.getBaseStatsPane().getButtonBack().setOnAction(actionEvent -> {
+			mainDefinitivo.getTabellone().checkbasi(giocatore);
+			mainDefinitivo.getTabellone().trovaBase(giocatore);
+			partitaStage.changeSelectedBase(b);
+		});
+	}
+	
+}

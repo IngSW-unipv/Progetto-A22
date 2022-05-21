52519a178e280b5177c4c5956dd88c0ca419dfff
MobGian <97670196+MobGian@users.noreply.github.com>
2022-05-21 16:17:50 +0200

    Correzione

--- src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/controller/SignupController.java
+++ src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/controller/SignupController.java
@@ -1,6 +1,7 @@
 package it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.controller;
 
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.prepartita.LoginView;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.prepartita.PrebattagliaView;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.prepartita.SignupView;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.PersistenceFacade;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.bean.UserAccount;
@@ -8,6 +9,14 @@ import javafx.event.ActionEvent;
 import javafx.stage.Stage;
 
 public class SignupController {
+	
+	
+	private UserAccount userAccount;
+	private PersistenceFacade persistenceFacade; 
+	//private LoginView loginView;
+	private PrebattagliaView prebattagliaView;
+	private SignupView signupView;
+	private Stage loginStage;
 	
 	private static SignupController signupController = null;
 	
@@ -22,17 +31,25 @@ public class SignupController {
 		return signupController;
 	}
 	
+	/**
+	 * 
+	 * @param signupView
+	 * @param persistenceFacade
+	 * @param loginStage
+	 * @param userAccount
+	 * Metodo associato al pulsante Signup per registrare un nuovo utende a Server Dominator
+	 */
 			
-	public void initSignup(SignupView signupView, PersistenceFacade persistenceFacade, Stage loginStage, UserAccount userAccount) {
+	public void initSignup() {
 		//String username = null;
 		//String passw = null;
 		//String email = null;
 		signupView.getSignupButtonLegit().setOnAction(ActionEvent ->{
 			
 			//userAccount=new UserAccount(signupView.getUsernameTextFieldSignup().getText(), signupView.getPasswordTextFieldSignup().getText());
-			boolean user = PersistenceFacade.getInstance().insertUserAccount(new UserAccount(signupView.getUsernameTextFieldSignup().getText(), 
-					signupView.getUsernameTextFieldSignup().getText(), signupView.getEmailTextField().getText()));
-			if(user) {
+			boolean us = persistenceFacade.getInstance().insertUserAccount(new UserAccount(signupView.getUsernameTextFieldSignup().getText(),  //togliere static a getInstance? 
+					signupView.getPasswordTextFieldSignup().getText(), signupView.getEmailTextField().getText()));
+			if(us) {
 				//boolean true passa logged in
 				signupView.getStage().close();
 				loginStage.show();
@@ -45,8 +62,15 @@ public class SignupController {
 	
 	}
 	
-	public void initChangeContext(Stage loginStage, ControllerFacade controllerFacade, SignupView signupView) {//cambiare scene, non fa il login, da ControllerFacade
-		controllerFacade.getSignupView().getLoginButtonChange().setOnAction(actionEvent ->{
+	/**
+	 * @param signupView
+	 * @param loginStage
+	 * Metodo abbitnato al pulsante Log in nella view di Signup, per passare alla view di Login
+	 * 
+	 */
+	
+	public void initChangeContext() {//cambiare scene, non fa il login, da ControllerFacade
+		signupView.getLoginButtonChange().setOnAction(actionEvent -> {
 			signupView.getStage().close();
 			loginStage.show();
 		});

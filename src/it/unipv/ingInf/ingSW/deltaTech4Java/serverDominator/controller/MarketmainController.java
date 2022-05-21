55df79ee4091f324ae1178d32217e792d8f1d6a3
TawaHabib <tawadrosgirgis.habib01@universitadipavia.it>
2022-05-21 11:47:51 +0200

    controller

--- src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/controller/MarketmainController.java
+++ src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/controller/MarketmainController.java
@@ -5,24 +5,19 @@ import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.partita.Partita
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.partita.PopUpFacade;
 
 public class MarketmainController {
+	private MainDefinitivo mainDefinitivo; 
+	private PopUpFacade popupFacade;
+	private PartitaStage partitaStage;
 	
-	private static MarketmainController marketmainController = null;
-	private MainDefinitivo mainDefinitivo;
-	
-	private MarketmainController() {
+	public MarketmainController(MainDefinitivo mainDefinitivo, PopUpFacade popupFacade, PartitaStage partitaStage) {
 		super();
+		this.mainDefinitivo = mainDefinitivo;
+		this.popupFacade = popupFacade;
+		this.partitaStage = partitaStage;
+		this.initMarket();
 	}
-	
-	public static MarketmainController getInstance() {
-		if(marketmainController == null)
-			marketmainController = new MarketmainController();
-		
-		return marketmainController;
-	}
-	
-	
-	
-	public void initMarket(MainDefinitivo mainDefinitivo, PopUpFacade popupFacade, PartitaStage partitaStage) {
+
+	public void initMarket() {
 		
 		/**
 		 * Metodo abbinato all'acquisto di risorse software e hardware dal mercato
@@ -43,10 +38,36 @@ public class MarketmainController {
 				mainDefinitivo.acquistoMercato(partitaStage.getSelectedNode().getPossessore(), quantitaHardware, "energia");
 			if(popupFacade.getPopUpMarket().getLivelloFirewall()>0)
 				mainDefinitivo.acquistoMercato(partitaStage.getSelectedNode().getPossessore(), quantitaHardware, "firewall");
-		});
+			this.partitaStage.getBaseStatsPane().make();
+			});
 		
 		
-	}//da aggiundere scrollPane?
+	}
+
+	public MainDefinitivo getMainDefinitivo() {
+		return mainDefinitivo;
+	}
+
+	public void setMainDefinitivo(MainDefinitivo mainDefinitivo) {
+		this.mainDefinitivo = mainDefinitivo;
+	}
+
+	public PopUpFacade getPopupFacade() {
+		return popupFacade;
+	}
+
+	public void setPopupFacade(PopUpFacade popupFacade) {
+		this.popupFacade = popupFacade;
+	}
+
+	public PartitaStage getPartitaStage() {
+		return partitaStage;
+	}
+
+	public void setPartitaStage(PartitaStage partitaStage) {
+		this.partitaStage = partitaStage;
+	}
+
 	
 	
 }

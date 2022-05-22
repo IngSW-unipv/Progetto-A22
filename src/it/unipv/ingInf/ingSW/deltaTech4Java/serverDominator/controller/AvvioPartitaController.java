4037e50f077e8706471ca1e0dd8d5961378194e9
MobGian <97670196+MobGian@users.noreply.github.com>
2022-05-22 12:22:51 +0200

    miglioramento

--- src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/controller/AvvioPartitaController.java
+++ src/it/unipv/ingInf/ingSW/deltaTech4Java/serverDominator/controller/AvvioPartitaController.java
@@ -1,8 +1,11 @@
 package it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.controller;
 
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.Base;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.MainDefinitivo;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.Nodo;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.giocatore.Classifica;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.model.giocatore.Giocatore;
+import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.persistence.bean.UserAccount;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.partita.PartitaStage;
 import it.unipv.ingInf.ingSW.deltaTech4Java.serverDominator.view.prepartita.PrebattagliaView;
 import javafx.event.ActionEvent;
@@ -14,6 +17,8 @@ public class AvvioPartitaController {
 	private PartitaStage partitaStage;
 	private PrebattagliaView prebattagliaView;
 	private ControllerFacade controllerFacade;
+	private MainDefinitivo mainDefinitivo;
+	private UserAccount userAccount;
 	
 	public AvvioPartitaController getInstance() {    // posso solo usare l'oggetto esistente
 		
@@ -38,10 +43,16 @@ public class AvvioPartitaController {
 		
 		prebattagliaView.getAvvioPartita().setOnAction(actionEvent -> {
 			
-			if(prebattagliaView.getGroup().getSelectedToggle().getToggleGroup()) //facile choice? getSelectedDifficolta.getSelectedToggle, come richiamo il giusto radiobutton?
-				
+			if(prebattagliaView.getGroup().getSelectedToggle().getToggleGroup() != null) //facile choice? getSelectedDifficolta.getSelectedToggle, come richiamo il giusto radiobutton?
 				partitaStage.getDimensioni().getValue();
-				
+				try {
+					mainDefinitivo.avvioPartita(10, 15, userAccount.getUsername(), userAccount.getMny());
+				} catch (InterruptedException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				controllerFacade.initEasyGame(null, mainDefinitivo, controllerFacade, userAccount, partitaStage);
+			
 		});
 		
 	}
